f122087f5d9034927c86ffa72da1a64ff408ebb5
Theodore Lee <bethejustice@naver.com>
2026-02-16 17:32:52 +0900

    feat: 앱 진입점 추가

---
+++ MusicTestApp/MusicTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTestAppApp.swift
+//  MusicTestApp
+//
+//  Created by theodore Lee on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
